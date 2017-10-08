73ea72e37dcd177e69bd585ab214b676a06cb439
wexr <wexr.kk@gmail.com>
2017-10-08 15:35:32 +0200

    Lidt flere kommandoer til chatbot.

--- src/wexr/Main.java
+++ src/wexr/Main.java
@@ -20,24 +20,33 @@ public class Main {
 
         while (! tekst.toLowerCase().contains("stop dig selv")) {
 
-            if(tekst.toLowerCase().contains("hvad er dit navn?"))
+            if(tekst.toLowerCase().contains("hvad er dit navn?")) {
                 System.out.println("Mit navn er Flotte Bot");
-
-            else if (tekst.toLowerCase().contains("hvor gammel er du?"))
-                    System.out.println(navn + " altså! Alder er bare et tal! tihi");
-
-                else if (tekst.toLowerCase().contains("hvem er den bedste lærer?"))
-                    System.out.println("Den bedste lærer er András som underviser i Programmering.");
+            }
+                else if (tekst.toLowerCase().contains("hvor gammel er du?")) {
+                System.out.println(navn + " altså! Alder er bare et tal! tihi");
+            }
+                else if (tekst.toLowerCase().contains("hvem er den bedste lærer?")) {
+                System.out.println("Den bedste lærer er András som underviser i Programmering.");
+            }
+                else if (tekst.toLowerCase().contains("hvad er 2 + 2?")){
+                System.out.println("Kunne godt være du skulle få dine skole penge tilbage. *wink* *wink*");
+            }
+                else if (tekst.toLowerCase().contains("hvordan bliver man god til java?")){
+                System.out.println("Øvelse før mester. Bare bliv ved! Det skal nok komme.");
+            }
                 else if (tekst.toLowerCase().contains("help")) {
 
                 System.out.println("Følgende kommandoer virker: ");
                 System.out.println("hvad er dit navn?");
                 System.out.println("hvor gammel er du?");
                 System.out.println("hvem er den bedste lærer?");
+                System.out.println("hvad er 2 + 2?");
+                System.out.println("hvordan bliver man god til java?");
                 System.out.println("stop dig selv");
 
             }
-            else
+                else
                 System.out.println("Jeg forstår desværre ikke hvad du siger");
 
             tekst = input.nextLine();
